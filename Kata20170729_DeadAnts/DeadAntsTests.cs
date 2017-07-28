9d4ecf2efa716c9aa563c409af32edef71e333db
cash <cash@cashwu.com>
2017-07-28 22:50:38 +0800

    init

--- Kata20170729_DeadAnts/DeadAntsTests.cs
+++ Kata20170729_DeadAnts/DeadAntsTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170729_DeadAnts
 {
     [TestClass]
-    public class UnitTest1
+    public class DeadAntsTests
     {
         [TestMethod]
         public void TestMethod1()
